e845b4079fd153937c4e5631220824e946eafe2f
Ashis Laha <ashis.laha@walmartlabs.com>
2018-06-15 11:43:41 +0530

    deleted unnecessary

--- build/Pods.build/Debug-iphoneos/Pods-AR in Retails.build/DerivedSources/Pods_AR_in_Retails_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char Pods_AR_in_RetailsVersionString[];
- extern const double Pods_AR_in_RetailsVersionNumber;
-
- const unsigned char Pods_AR_in_RetailsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_AR_in_Retails  PROJECT:Pods-1" "\n";
- const double Pods_AR_in_RetailsVersionNumber __attribute__ ((used)) = (double)1.;
